4c1342cf7c200a7e3e6ada59c3fdfe5c3216d42a
uklewis124 <lewisastondavies1@gmail.com>
2024-01-08 09:18:01 +0000

    fdsgz

--- src/bulk_rename/setup.py
+++ src/bulk_rename/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bulk_rename',
-    version='0.0.1',
+    version='0.1.0',
     author='Lewis Davies',
     author_email='lewisastondavies1@gmail.com',
     description='A package to rename multiple files and folders',
